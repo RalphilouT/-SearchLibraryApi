236e99216e117a920a877c7a0c0cf0e7863d0196
Ralphilou Tatoy <ralphilou24@gmail.com>
2023-12-17 09:01:36 -0800

    API with api key security and mongodb

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -2,42 +2,33 @@ package com.example.library;
 
 import com.example.library.Entity.BookEntity;
 import com.example.library.repository.BookRepository;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class LibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
-	@Component
-	public class CorsFilter extends OncePerRequestFilter {
 
-		@Override
-		protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
-										final FilterChain filterChain) throws ServletException, IOException {
-			response.addHeader("Access-Control-Allow-Origin", "*");
-			response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
-			response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-			response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
-			response.addHeader("Access-Control-Allow-Credentials", "true");
-			response.addIntHeader("Access-Control-Max-Age", 10);
-			filterChain.doFilter(request, response);
-		}
-	}
+
+
+
 	@Bean
 	CommandLineRunner runner(BookRepository repository, MongoTemplate mongoTemplate){
 		return args -> {
